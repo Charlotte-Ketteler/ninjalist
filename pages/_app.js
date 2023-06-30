1e054cc2259bd253fa24fdbd0834cc53c887b33d
Charlotte Ketteler <ketteler.cha@gmail.com>
2023-06-30 18:46:10 +0200

    FrameworkDone

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,16 @@
+import Layout from "@/components/Layout";
 import GlobalStyle from "../styles";
+
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+
+    <Layout>
+    <Component {...pageProps} />
+    </Layout>
       <GlobalStyle />
-      <Component {...pageProps} />
+     
     </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,16 @@
-import Footer from "../components/Footer";
-import Navbar from "../components/Navbar";
+
 import Link from "next/link";
 
 export default function HomePage() {
   return (
     <div>
-      <Navbar/>
+     
       <h1>Homepage</h1>
       <p>Aliqua aute laborum pariatur velit officia quis ex commodo aute officia. Ex commodo officia dolore quis irure voluptate fugiat adipisicing veniam do in eu ex voluptate. Occaecat minim id laboris esse sunt Lorem eiusmod laboris occaecat qui nisi cillum dolor. Occaecat quis voluptate qui do nostrud veniam mollit sint deserunt non dolor deserunt reprehenderit aliqua. Et deserunt Lorem sit pariatur nostrud ipsum pariatur nostrud adipisicing eiusmod ad anim.</p>
       <p>Aliqua aute laborum pariatur velit officia quis ex commodo aute officia. Ex commodo officia dolore quis irure voluptate fugiat adipisicing veniam do in eu ex voluptate. Occaecat minim id laboris esse sunt Lorem eiusmod laboris occaecat qui nisi cillum dolor. Occaecat quis voluptate qui do nostrud veniam mollit sint deserunt non dolor deserunt reprehenderit aliqua. Et deserunt Lorem sit pariatur nostrud ipsum pariatur nostrud adipisicing eiusmod ad anim.</p>
     <Link href="/ninjas">
     See Ninja Listing</Link>
-      <Footer/>
+     
     </div>
   );
 }
